b8a94d4a348af949379c400b0c7f48d91d78e6f2
Yury Tvardovsky <oziabr@gmail.com>
2017-02-07 21:52:09 +0200

    Create gps-navigation.js

---
+++ gps-navigation.js
@@ -0,0 +1,26 @@
+// 3kyu
+// https://www.codewars.com/kata/5244ab738978478c1800002e
+
+function navigate(n, roads, start, finish) {
+  if (start == finish) return [start]
+  var result, routes = [[0, start]]
+  while (n-- && routes.length) {
+    routes = routes.reduce(function (res, rt, i) {
+      return res.concat(
+        roads
+        .filter(r => r.from == rt[rt.length-1])
+        .map(function (r) {
+          var nr = rt.concat(r.to)
+          nr[0] += r.drivingTime
+          return nr
+        })
+      )
+    }, []).filter(function (rt) {
+      if (result && rt[0] > result[0]) return false
+      if (finish != rt[rt.length-1]) return true
+      result = result || rt
+      result = rt[0] < result[0] ? rt : result
+    })
+  }
+  return result ? result.slice(1) : null
+}
